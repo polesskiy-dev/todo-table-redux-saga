0d69f13d9294731e864c8c255c0e5564db0f2c31
Artem Polisskyi <polesskiy.dev@gmail.com>
2016-09-05 13:18:51 +0300

    add enzyme form test

--- src-frontend/components/toddo-app/new-todo-form/NewTodoForm.spec.js
+++ src-frontend/components/toddo-app/new-todo-form/NewTodoForm.spec.js
@@ -1,7 +1,11 @@
+/**
+ * Test form with enzyme
+ *
+ * @see http://airbnb.io/enzyme/docs/api
+ */
 import "babel-polyfill";
 import React from 'react'
 import {mount} from 'enzyme'
-import {Map, List} from 'immutable'
 import configureStore from 'redux-mock-store'
 import chai from 'chai'
 import NewTodoForm from './NewTodoForm'
@@ -13,12 +17,29 @@ describe('<NewTodoForm />', () => {
         const typeTextEvent = {target: {value: 'hello'}};
         const wrapper = mount(<NewTodoForm store={store}/>);
 
-        const textInput = wrapper.find('input');
+        const textInput = wrapper.find('input').first();
+        //wrapper.find('input').render();
         textInput.simulate('focus');
         textInput.simulate('change', typeTextEvent);
-        console.log(textInput.get(0).value)
+        textInput.simulate('blur');
 
-        // chai.expect(wrapper.find("form").hasClass("has-success")).to.be.true;
-        chai.expect(true).to.be.true;
+        console.log(document.getElementsByTagName('input'));
+
+        chai.expect(textInput.hasClass("form-control-success")).to.be.true;
+    })
+
+    it("Should have danger class while input is unfocused with empty value", ()=> {
+        const mockStore = configureStore();
+        const store = mockStore({});
+        const emptyTextEvent = {target: {value: ''}};
+        const wrapper = mount(<NewTodoForm store={store}/>);
+
+        const textInput = wrapper.find('input');
+        textInput.simulate('focus');
+        textInput.simulate('change', emptyTextEvent);
+        textInput.simulate('blur');
+
+        chai.expect(textInput.hasClass("form-control-danger")).to.be.true;
     })
 });
+
